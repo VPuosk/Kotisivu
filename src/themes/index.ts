96f67a45f4133711656e7301eabf8dcf96c18df5
VPuosk <vesa.puoskari@gmail.com>
2021-11-03 07:04:38 +0200

    aloitus...

---
+++ src/themes/index.ts
@@ -0,0 +1,49 @@
+
+const theme = {
+  colors: {
+    textPrimary: '#24292e',
+    textSecondary: '#586069',
+    primary: '#0366d6',
+    barBackground: '#253030',
+    barFont: '#ccc',
+    background: '#CCC',
+    repositoryBackground: '#eee',
+    languageBackground: '#55d',
+    errorTextColor: '#ee2222',
+  },
+  fontSizes: {
+    body: 14,
+    subheading: 16,
+  },
+  fonts: {
+    main: 'System',
+    android: 'Roboto',
+    ios: 'Arial',
+  },
+  fontWeights: {
+    normal: '400',
+    bold: '700',
+  },
+
+  bigbox: {
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    backgroundColor: '#22d',
+    color: '#ccc',
+    padding: 10,
+    margin: 5
+  },
+
+  bigredbox: {
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    backgroundColor: '#d22',
+    color: '#ccc',
+    padding: 10,
+    margin: 5
+  },
+};
+
+export default theme;
